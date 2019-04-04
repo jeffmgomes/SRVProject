24d85feeb148ea523295fcaba8979d6c964ea078
Jefferson Gomes <jefferson.mg@hotmail.com>
2019-04-04 20:42:11 +1030

    First commit - Upload code

---
+++ TAFEBuddy_SRV_Prototype/views/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TAFEBuddy_SRV_Prototype.views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Login : Page
+    {
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (usernameTextBox.Text == "student")
+            {
+                this.Frame.Navigate(typeof(views.SRV_student));
+            }
+            else if (usernameTextBox.Text == "admin")
+            {
+                this.Frame.Navigate(typeof(views.SRV_admin));
+            }
+        }
+    }
+}
